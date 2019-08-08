363bc99098ae442005bb67c21b6d44f2044e95ce
Aliaksandr Laskauneu <Aliaksandr_Laskauneu@epam.com>
2019-08-08 12:41:54 +0300

    Post (put) request implementation

--- Tweetbook/Controllers/V1/PostsController.cs
+++ Tweetbook/Controllers/V1/PostsController.cs
@@ -28,6 +28,24 @@ namespace Tweetbook.Controllers.V1
             return Ok(post);
         }
 
+        [HttpPut(ApiRoutes.Posts.Update)]
+        public IActionResult Update([FromRoute]Guid postId, [FromBody] UpdatePostRequest request)
+        {
+            var post = new Post
+            {
+                Id = postId,
+                Name = request.Name
+            };
+
+            var updated = _postService.UpdatePost(post);
+
+            if(updated)
+                return Ok(post);
+
+            return NotFound();
+
+        }
+
         [HttpGet(ApiRoutes.Posts.GetAll)]
         public IActionResult GetAll()
         {
